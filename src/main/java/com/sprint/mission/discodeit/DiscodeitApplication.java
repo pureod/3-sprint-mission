f1f3fe828a16b8bf493ba6b622aa313af24bb4a3
pureod <sjo06102@gmail.com>
2025-05-09 09:45:21 +0900

    Implement: AuthController 구현 완료

--- src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -28,36 +28,10 @@ import java.util.Optional;
 
 @SpringBootApplication
 public class DiscodeitApplication {
-	static User setupUser(UserService userService) {
-		UserCreateRequest request = new UserCreateRequest("woody", "woody@codeit.com", "woody1234");
-		User user = userService.create(request, Optional.empty());
-		return user;
-	}
-
-	static Channel setupChannel(ChannelService channelService) {
-		PublicChannelCreateRequest request = new PublicChannelCreateRequest("공지", "공지 채널입니다.");
-		Channel channel = channelService.create(request);
-		return channel;
-	}
-
-	static void messageCreateTest(MessageService messageService, Channel channel, User author) {
-		MessageCreateRequest request = new MessageCreateRequest("안녕하세요.", channel.getId(), author.getId());
-		Message message = messageService.create(request, new ArrayList<>());
-		System.out.println("메시지 생성: " + message.getId());
-	}
 
 	public static void main(String[] args) {
 
-		ConfigurableApplicationContext context = SpringApplication.run(DiscodeitApplication.class, args);
-		// 서비스 초기화
-		UserService userService = context.getBean(UserService.class);
-		ChannelService channelService = context.getBean(ChannelService.class);
-		MessageService messageService = context.getBean(MessageService.class);
+		SpringApplication.run(DiscodeitApplication.class, args);
 
-		// 셋업
-		User user = setupUser(userService);
-		Channel channel = setupChannel(channelService);
-		// 테스트
-		messageCreateTest(messageService, channel, user);
 	}
 }
---
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -0,0 +1,48 @@
+package com.sprint.mission.discodeit.controller;
+
+import com.sprint.mission.discodeit.dto.data.UserDto;
+import com.sprint.mission.discodeit.dto.request.LoginRequest;
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.service.AuthService;
+import com.sprint.mission.discodeit.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestPart;
+
+import java.util.NoSuchElementException;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final AuthService authService;
+    private final UserService userService;
+
+    @RequestMapping(
+            path = "/login"
+            , method = RequestMethod.POST
+            , consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<?> login(
+            @RequestPart LoginRequest loginRequest) {
+
+        try {
+            User user = authService.login(loginRequest);
+            UserDto userDto = userService.find(user.getId());
+
+            return ResponseEntity.status(HttpStatus.OK).body(userDto);
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("존재하지 않는 회원입니다.");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body("비밀번호가 일치하지 않습니다.");
+        }
+    }
+}
