a34d9d55f320cbf6998b9c3965c7bd065924b0a0
pureod <sjo06102@gmail.com>
2025-05-10 18:00:27 +0900

    Implement: BinaryContentController 구현 완료

---
+++ src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
@@ -0,0 +1,48 @@
+package com.sprint.mission.discodeit.controller;
+
+import com.sprint.mission.discodeit.entity.BinaryContent;
+import com.sprint.mission.discodeit.service.BinaryContentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/api/binaryContent")
+public class BinaryContentController {
+
+    private final BinaryContentService binaryContentService;
+
+    @RequestMapping(
+            path = "/find"
+            , method = RequestMethod.GET
+    )
+    @ResponseBody
+    public ResponseEntity<BinaryContent> findById(
+            @RequestParam("id") UUID id
+    ) {
+        BinaryContent content = binaryContentService.find(id);
+
+        return ResponseEntity.status(HttpStatus.OK).body(content);
+    }
+
+    @RequestMapping(
+            path = "/findAll"
+            , method = RequestMethod.GET
+    )
+    @ResponseBody
+    public ResponseEntity<?> findAll(
+            @RequestParam("ids") List<UUID> ids
+    ) {
+        List<BinaryContent> contents = binaryContentService.findAllByIdIn(ids);
+
+        return ResponseEntity.status(HttpStatus.OK).body(contents);
+    }
+
+
+}
