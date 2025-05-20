617d66824451ddf0a77a7dcc9aec6e21398b1033
pureod <sjo06102@gmail.com>
2025-05-20 15:19:40 +0900

    Modified: 예외 전역화 - ErrorResponse DTO를 추가하여 response body의 일관성 부여

--- src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
@@ -1,52 +1,63 @@
 package com.sprint.mission.discodeit.exception;
 
+import com.sprint.mission.discodeit.dto.response.ErrorResponse;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.NoSuchElementException;
-
-@ControllerAdvice
-@ResponseBody
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
+  private ResponseEntity<ErrorResponse> sendErrorResponse(HttpStatus status, String message) {
+    return ResponseEntity.status(status).body(ErrorResponse.of(status, message));
+  }
+
+  // 잘못된 요청을 하는 경우
   @ExceptionHandler(IllegalArgumentException.class)
-  public ResponseEntity<String> handleException(IllegalArgumentException e) {
-    return ResponseEntity
-        .status(HttpStatus.BAD_REQUEST)
-        .body(e.getMessage());
+  public ResponseEntity<ErrorResponse> handleException(IllegalArgumentException e) {
+
+    return sendErrorResponse(HttpStatus.BAD_REQUEST, e.getMessage());
   }
 
+  // 데이터가 존재하지 않을 경우
   @ExceptionHandler(NoSuchElementException.class)
-  public ResponseEntity<String> handleException(NoSuchElementException e) {
-    return ResponseEntity
-        .status(HttpStatus.NOT_FOUND)
-        .body(e.getMessage());
+  public ResponseEntity<ErrorResponse> handleException(NoSuchElementException e) {
+
+    return sendErrorResponse(HttpStatus.NOT_FOUND, e.getMessage());
   }
 
+  // 파일 IO에 문제가 생겼을 경우
+  @ExceptionHandler(RuntimeException.class)
+  public ResponseEntity<ErrorResponse> runtimeExceptionExceptionHandler(IOException e) {
+    return sendErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+  }
+
+  // 나머지 예외 처리
   @ExceptionHandler(Exception.class)
-  public ResponseEntity<String> handleException(Exception e) {
-    return ResponseEntity
-        .status(HttpStatus.INTERNAL_SERVER_ERROR)
-        .body(e.getMessage());
+  public ResponseEntity<ErrorResponse> handleException(Exception e) {
+
+    return sendErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
   }
 
-  @ExceptionHandler(MethodArgumentNotValidException.class)
-  public ResponseEntity<Map<String, String>> handleValidationExceptions(
-      MethodArgumentNotValidException e) {
-    Map<String, String> errors = new HashMap<>();
-    e.getBindingResult().getAllErrors().forEach((error) -> {
-      String fieldName = ((FieldError) error).getField();
-      String errorMessage = error.getDefaultMessage();
-      errors.put(fieldName, errorMessage);
-    });
-    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
-  }
+// //유효성 검증 오류 시 예외 처리
+//  @ExceptionHandler(MethodArgumentNotValidException.class)
+//  public ResponseEntity<Map<String, String>> handleValidationExceptions(
+//      MethodArgumentNotValidException e) {
+//    Map<String, String> errors = new HashMap<>();
+//    e.getBindingResult().getAllErrors().forEach((error) -> {
+//      String fieldName = ((FieldError) error).getField();
+//      String errorMessage = error.getDefaultMessage();
+//      errors.put(fieldName, errorMessage);
+//    });
+//
+//    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+//  }
 
 }
