e7ac7756d1682df96006371874fa37ba1e391dc1
pureod <sjo06102@gmail.com>
2025-05-22 22:03:17 +0900

    Modified: 유효성 검사 예외 처리 메소드를 업데이트된 형태의 전역 예외처리에 맞게 수정

--- src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
@@ -5,11 +5,13 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
@@ -46,18 +48,14 @@ public class GlobalExceptionHandler {
     return sendErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
   }
 
-// //유효성 검증 오류 시 예외 처리
-//  @ExceptionHandler(MethodArgumentNotValidException.class)
-//  public ResponseEntity<Map<String, String>> handleValidationExceptions(
-//      MethodArgumentNotValidException e) {
-//    Map<String, String> errors = new HashMap<>();
-//    e.getBindingResult().getAllErrors().forEach((error) -> {
-//      String fieldName = ((FieldError) error).getField();
-//      String errorMessage = error.getDefaultMessage();
-//      errors.put(fieldName, errorMessage);
-//    });
-//
-//    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
-//  }
+  //유효성 검증 오류 시 예외 처리
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<ErrorResponse> handleValidationExceptions(
+      MethodArgumentNotValidException e) {
+    String message = e.getBindingResult().getFieldErrors().stream()
+        .map(error -> error.getField() + ": " + error.getDefaultMessage())
+        .collect(Collectors.joining(", "));
+    return sendErrorResponse(HttpStatus.BAD_REQUEST, e.getMessage());
+  }
 
 }
