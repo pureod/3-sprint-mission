050557c03555d406f4c36e8a75d43e4e9818912a
pureod <sjo06102@gmail.com>
2025-05-29 16:36:22 +0900

    Add: 파일 저장을 위한 BinaryContentStorage 인터페이스 생성

--- src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
+++ src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
@@ -30,14 +30,10 @@ public class BinaryContent extends BaseEntity implements Serializable {
   @Column(name = "content_type", nullable = false)
   private String contentType;
 
-  @Column(name = "bytes", nullable = false)
-  private byte[] bytes;
-
-  public BinaryContent(String fileName, Long size, String contentType, byte[] bytes) {
+  public BinaryContent(String fileName, Long size, String contentType) {
     super();
     this.fileName = fileName;
     this.size = size;
     this.contentType = contentType;
-    this.bytes = bytes;
   }
 }
