760980849dd05e0d8caaa37cd4d999db34799314
pureod <sjo06102@gmail.com>
2025-08-05 09:41:53 +0900

    feat: Spring Security 기본 설정 및 필터 목록 디버깅 추가

---
+++ src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.sprint.mission.discodeit.config;
+
+import java.util.List;
+import java.util.stream.IntStream;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public CommandLineRunner debugFilterChain(SecurityFilterChain filterChain) {
+
+        return args -> {
+            int filterSize = filterChain.getFilters().size();
+
+            List<String> filterNames = IntStream.range(0, filterSize)
+                .mapToObj(idx -> String.format("\t[%s/%s] %s", idx + 1, filterSize,
+                    filterChain.getFilters().get(idx).getClass()))
+                .toList();
+
+            System.out.println("현재 적용된 필터 체인 목록:");
+            filterNames.forEach(System.out::println);
+        };
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.build();
+    }
+
+}
