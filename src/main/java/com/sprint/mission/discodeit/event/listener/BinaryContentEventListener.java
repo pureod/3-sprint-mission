4ce7cd6b4a660dbdd6271b7f6cd0ce673fc23848
pureod <sjo06102@gmail.com>
2025-08-25 09:46:51 +0900

    feat: 이벤트 리스너 비동기 처리 및 비동기 환경에서의 MDC 로깅 보완

--- src/main/java/com/sprint/mission/discodeit/event/listener/BinaryContentEventListener.java
+++ src/main/java/com/sprint/mission/discodeit/event/listener/BinaryContentEventListener.java
@@ -7,6 +7,7 @@ import com.sprint.mission.discodeit.storage.BinaryContentStorage;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -19,6 +20,7 @@ public class BinaryContentEventListener {
     private final BinaryContentStorage binaryContentStorage;
     private final BinaryContentService binaryContentService;
 
+    @Async("asyncExecutor")
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handle(BinaryContentCreatedEvent event) {
 
--- src/main/java/com/sprint/mission/discodeit/event/listener/NotificationEventListener.java
+++ src/main/java/com/sprint/mission/discodeit/event/listener/NotificationEventListener.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -16,11 +17,12 @@ import org.springframework.transaction.event.TransactionalEventListener;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class NotificationRequiredEventListener {
+public class NotificationEventListener {
 
     private final NotificationService notificationService;
     private final ReadStatusRepository readStatusRepository;
 
+    @Async("asyncExecutor")
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void on(MessageCreatedEvent event) {
 
@@ -43,6 +45,7 @@ public class NotificationRequiredEventListener {
             });
     }
 
+    @Async("asyncExecutor")
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void on(RoleUpdatedEvent event) {
 
